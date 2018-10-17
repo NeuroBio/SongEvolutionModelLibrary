5f57106495ee803245d8b44549c4e313e3e05e6d
Cristina Robinson <NeuroBio@users.noreply.github.com>
2018-10-17 13:15:53 -0500

    Added Invasion Simulation (only for Learning atm)

--- Simulations.cs
+++ Simulations.cs
@@ -1,6 +1,6 @@
 using System;
 //using System.Collections.Generic;
-//using System.Linq;
+using System.Linq;
 //using System.Diagnostics;
 //using System.Threading;
 //using MathNet.Numerics;
@@ -33,8 +33,43 @@ namespace SongEvolutionModelLibrary
             }
             return(SimData);
         }
+        public static InvasionData InvasionLrnThrsh(SimParams par, float InvaderStat, int numInvaders=1, int burnIn=500){
+            Population Pop = new Population(par);
+            //Get to Equilibrium         
+            for(int i=0;i<burnIn;i++){
+                Pop = BirthDeathCycle.Step(par,Pop);
+            }
+            
+            //Invade
+            int[] InvaderIndex = par.randomSampleEqualNoReplace(Enumerable.Range(0,par.NumBirds).ToList(), numInvaders);
+            for(int i=0;i<numInvaders;i++){
+                Pop.Age[InvaderIndex[i]] = 1;
+                Pop.LearningThreshold[InvaderIndex[i]] = InvaderStat;
+            }
+
+            //Postinvasion run
+            int Counter = 1;
+            int Categories = 2;
+            while(Categories != 1){
+                Counter += 1;
+                Pop = BirthDeathCycle.Step(par,Pop);
+                if(Counter == 400){
+                    break;
+                }else{
+                    Categories = Pop.LearningThreshold.Distinct().Count();
+                }
+            }
+            InvasionData Results = new InvasionData(Counter, Pop.LearningThreshold.Average());
+            return(Results);
+        }
+        public struct InvasionData{
+            public int Steps;
+            public float TraitAve;
+            public InvasionData(int steps = default(int), float traitAve=default(float)){
+                Steps = steps;
+                TraitAve = traitAve;
+            }
+        }
     }
-
-
 
 }
